27c374ec043692575de7f29eb7c2e4b2c45315e5
JavaHunk2020 <javahunk2020@gmail.com>
2023-08-31 05:55:01 +0530

    Angular Day-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'photo-album-app';
+  message  = 'Welcome Jee Owow Learning Angular!!!';
+
+  /**
+   * this is logic 
+   */
+  public process(username : any , password : any, email:any) : void {
+      //alert("Hello  =  "+username.value);
+      this.message = 'Hello  =  '+username.value+' , Password = '+password.value+' and email = '+email.value;
+  }
+
+  public display(sername : any , password : any, email:any) : void {
+    //alert("Hello  =  "+username.value);
+  
+  }
 }
